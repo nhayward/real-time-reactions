98b6b7065a472125de1b5f354a86ba93c3a11cb2
Nick Hayward <nhayward@everspringpartners.com>
2016-05-07 14:57:48 -0500

    the existing code from my local machine

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	var i = reactions.length - 1;
+
+	function showReactions() {
+		if ( i == -1 ) {
+			location.reload();
+		}
+		$("#reaction").text("\"" + reactions[i].reaction + "\"");
+		$("#name").text("-" + reactions[i].name);
+		$("#reaction-container").height($("#reaction").height() + 46);
+		$("#reaction-container").css('visibility','visible').hide().fadeIn(3000);
+		setTimeout(function() {
+			$("#reaction-container").fadeOut(3000);
+		}, 10000);
+		setTimeout(function() {
+			i--;
+			setTimeout(showReactions(), 3000);
+		}, 13000);
+	}
+
+	showReactions();
+
+	$("#comment_form").on("submit", function () {
+		if ($("#your-name").val() === "" || $("#message").val() === "") {
+			alert("Please fill out all fields!");
+		} else {
+			reactions.push({reaction: $("#message").val(), name: $("#your-name").val()});
+			$.ajax({
+				type: "POST",
+				url: "update_data.php",
+				data: {reactions: JSON.stringify(reactions)}
+            });
+			$("#comment_form").reset();
+		}
+		return false;
+	});
+});
---
+++ update_data.php
@@ -0,0 +1,13 @@
+<?php
+
+$myfile = fopen("js/data.js", "w") or die("Unable to open file!");
+$arrayStart = "var reactions = ";
+fwrite($myfile, $arrayStart);
+$data = $_POST["reactions"];
+echo $data;
+fwrite($myfile, $data);
+$arrayEnd = ";";
+fwrite($myfile, $arrayEnd);
+fclose($myfile);
+
+?>

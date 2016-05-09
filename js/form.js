0445fd4b4119da0f50bf4af0c5be2d1bb2953fc7
Nick Hayward <nhayward@Nicks-MacBook-Pro.local>
2016-05-08 19:53:39 -0500

    changing update_data.php to updateData.php to keep the naming conventions consistent

--- js/form.js
+++ js/form.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 			reactions.push({reaction: $("#message").val(), name: $("#your-name").val()});
 			$.ajax({
 				type: "POST",
-				url: "php/update_data.php",
+				url: "php/updateData.php",
 				data: {reactions: JSON.stringify(reactions)}
             });
 			$("#comment_form").trigger("reset");

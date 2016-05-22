ae45d751a910b40a1ceac1d3ec4360130c4bb894
Nick Hayward <nhayward@Nicks-MacBook-Pro-2.local>
2016-05-22 14:22:09 -0500

    logic to get tweets and store them into an array to iterate through

--- js/reactions.js
+++ js/reactions.js
@@ -20,6 +20,32 @@ $(document).ready(function() {
 
 	// showReactions();
 
+	var tweets = [];
+
+	function gatherTweets(tweet) {
+		tweets.push(tweet);
+	}
+
+	var j = 0;
+
+	function showTweets() {
+		if (j > tweets.length - 1) {
+			j = 0;
+		}
+		$("#reaction").html(tweets[j]);
+		// $("#reaction-container").height($("#reaction").height() + 46);
+		$("#reaction-container").css('visibility','visible').hide().fadeIn(3000);
+		setTimeout(function() {
+			$("#reaction-container").fadeOut(3000);
+		}, 10000);
+		setTimeout(function() {
+			j++;
+			setTimeout(showTweets(), 3000);
+		}, 13000);
+	}
+
+	showTweets();
+
 	$.ajax({
 		type: "GET",
 		url: "php/hashtagGet.php"
@@ -30,10 +56,10 @@ $(document).ready(function() {
 				type: "POST",
 				url: "php/tweetGet.php",
 				dataType: "json",
-				data: ({tweetId: $(this)[0].id_str})
-			})
-			.done(function(tweetData) {
-				console.log(tweetData.html);
+				data: ({tweetId: $(this)[0].id_str}),
+				success: function(tweetData) {
+					gatherTweets(tweetData.html);
+				}
 			});
 		});
 	});

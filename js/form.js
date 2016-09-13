0b5583ce9de987234c104aab419f6e00e30812c6
Nick Hayward <nhayward@Nicks-MacBook-Pro-2.local>
2016-09-12 23:28:20 -0400

    prettying up the form interaction on submit

--- js/form.js
+++ js/form.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	$("#comment_form").on("submit", function () {
 		if ($("#your-name").val() === "" || $("#message").val() === "") {
-			alert("Please fill out all fields!");
+			$("#submitMessage").css("color", "#a94442");
+			$("#submitMessage").html("Please fill out all fields!");
+			$("#submitMessage").fadeIn(500);
+			setTimeout(function() {
+				$("#submitMessage").fadeOut(1000);
+			}, 5000);
 		} else {
 			reactions.push({reaction: $("#message").val(), name: $("#your-name").val()});
 			$.ajax({
@@ -10,6 +15,12 @@ $(document).ready(function() {
 				data: {reactions: JSON.stringify(reactions)}
             });
 			$("#comment_form").trigger("reset");
+			$("#submitMessage").css("color", "#3c763d");
+			$("#submitMessage").html("Thanks for submitting your thoughts!");
+			$("#submitMessage").fadeIn(500);
+			setTimeout(function() {
+				$("#submitMessage").fadeOut(1000);
+			}, 5000);
 		}
 		return false;
 	});
